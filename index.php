027a5bca8c4f87f8724e2419f54546706d569d91
Matt Zanchelli <matt@mdznr.com>
2012-02-11 22:15:18 -0500

    View individual posts!

--- index.php
+++ index.php
@@ -15,18 +15,25 @@
 	<h3 class="blogtitle">Matt Zanchelli</h3>
 	<div id="posts">
 			<?php
-				include("includes/functions.php");
-				$dir = "posts";
-				$postsPerPage = 5;
-				$postsPerPage = $_GET["postsPerPage"];
-				$page = 0;
-				$page = $_GET["page"];
-				$offset = $postsPerPage * $page;
+				include("includes/functions.php");	// Is it necessary for linking to universal functions if there's only going to be one .php file using them? Hmm...
+				$dir = "posts";	//	Directory for storing posts
+				$postsPerPage = 5;	//	Will be configurable
+				$postsPerPage = $_GET["postsPerPage"];	//	Overrides with URL arguments
+				$page = 0;	//	Page # (Starts with 0)
+				$page = $_GET["page"];	//	Overrides with URL arguments
+				$post = null;	//	Individual post (no specific post set by default)
+				$post = $_GET["post"];	//	Individual posts
+				$offset = $postsPerPage * $page;	//	Calculates the offset for loading posts 
 				opendir( $dir );
 				$posts = glob( $dir . '/*.txt' );
 				usort($posts, earlierPost);
-				for ( $i=$offset; $i<(count($posts)+$offset) && $i<($postsPerPage+$offset); $i++ ) {
-					echo "<div class='content'>" . file_get_contents( $posts[$i] ) . "</div>";
+				if ( $post ) {
+					echo "<div class='content'>" . file_get_contents( $dir . '/' . $post ) . "</div>";
+				}
+				else {
+					for ( $i=$offset; $i<(count($posts)+$offset) && $i<($postsPerPage+$offset); $i++ ) {
+						echo "<div class='content'>" . file_get_contents( $posts[$i] ) . "</div>";
+					}
 				}
 			?>
 	</div>
